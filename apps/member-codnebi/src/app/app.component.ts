8fa6fbde207fe872f5922b64c39ab7af65f69784
Konstantine Kirkitadze <74476606+kotekirkitadze@users.noreply.github.com>
2023-11-10 20:34:35 +0400

    add main and blog sections

--- apps/member-codnebi/src/app/app.component.ts
+++ apps/member-codnebi/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LandingComponent } from './pages/landing.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule,
+    LandingComponent],
   selector: 'codnebi-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
