a93e638f6c6e8c826f1003d0c4c132118bee3082
Konstantine Kirkitadze <74476606+kotekirkitadze@users.noreply.github.com>
2023-11-11 18:40:01 +0400

    add logo

--- apps/member-codnebi/src/app/app.component.ts
+++ apps/member-codnebi/src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { LandingComponent } from './pages/landing-page/landing.component';
 import { SideNavComponent } from './components/side-nav-component/side-nav.component';
 import { HeaderComponent } from './components/header-component/header.component';
 
 @Component({
   standalone: true,
-  imports: [
-    NxWelcomeComponent,
-    RouterModule,
-    LandingComponent,
-    SideNavComponent,
-    HeaderComponent,
-  ],
+  imports: [RouterModule, LandingComponent, SideNavComponent, HeaderComponent],
   selector: 'codnebi-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
