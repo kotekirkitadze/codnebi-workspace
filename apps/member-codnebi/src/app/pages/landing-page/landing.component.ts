153ac2d2c93006d299026654567fb716028f4d7f
Konstantine Kirkitadze <74476606+kotekirkitadze@users.noreply.github.com>
2023-11-11 16:15:17 +0400

    add side nav

--- apps/member-codnebi/src/app/pages/landing-page/landing.component.ts
+++ apps/member-codnebi/src/app/pages/landing-page/landing.component.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { BlogCardComponent } from '../../components/blog-card-component/blog-card.component';
 import { Router } from '@angular/router';
 import { HeaderComponent } from '../../components/header-component/header.component';
+import { BurgerMenuComponent } from '../../components/burger-menu-component/burger-menu.component';
 
 @Component({
   selector: 'codnebi-workspace-landing',
   standalone: true,
-  imports: [CommonModule, BlogCardComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    BlogCardComponent,
+    HeaderComponent,
+    BurgerMenuComponent,
+  ],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
